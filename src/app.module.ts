a5a964b5bdc2104f6cdfe010a2c72430ff021fb2
Patlix Studio <patlixstudio@gmail.com>
2025-12-01 10:13:28 +0300

    auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
@@ -6,12 +7,13 @@ import { AuthModule } from './auth/auth.module';
 import { User } from './users/user.entity';
 import { AuthController } from './auth/auth.controller';
 
+
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DATABASE_HOST || 'db',
-      port: +process.env.DATABASE_PORT || 5432,
+      port: parseInt(process.env.DATABASE_PORT || '5432', 10),
       username: process.env.DATABASE_USER || 'dev',
       password: process.env.DATABASE_PASSWORD || 'devpassword',
       database: process.env.DATABASE_NAME || 'vondel',
